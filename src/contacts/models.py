5782ba4bf6fe928b32b899b66a530a35eb44817c
oleksii_HP <oleksii@example.com>
2025-09-12 00:06:25 +0300

    1_2

---
+++ src/contacts/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Date
+
+from config.db import Base
+
+
+
+class Contact(Base):
+    __tablename__ = "contact"
+
+
+    id:Mapped(int) = mapped_column(Integer, primary_key=True, index=True)
+    first_name:Mapped[str] = mapped_column(String, index=True)
+    last_name:Mapped[str] = mapped_column(String, index=True)
+    e_mail: Mapped[str] = mapped_column(String, unique=True index=True)
+    birth_date: Mapped[Date] = mapped_column(Date)
+    additional_info: Mapped[str | None] = mapped_column(String, nulable=True)
